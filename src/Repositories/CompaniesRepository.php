a88ed3b6fc4448baa2400ff8476ee30320d338e2
Rob Pungello <rob@pdfsystems.com>
2024-08-30 11:22:30 -0400

    Add missing throw declarations for CompaniesRepository.php

--- src/Repositories/CompaniesRepository.php
+++ src/Repositories/CompaniesRepository.php
@@ -5,19 +5,32 @@ namespace Pdfsystems\OrderTrackSdk\Repositories;
 use GuzzleHttp\Exception\GuzzleException;
 use Pdfsystems\OrderTrackSdk\Dtos\Company;
 use Pdfsystems\OrderTrackSdk\Dtos\Representation;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
 class CompaniesRepository extends Repository
 {
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
     public function create(Company $company): Company
     {
         return $this->client->postDto('api/teams', $company);
     }
 
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
     public function find(int $id): Company
     {
         return $this->client->getDto("api/teams/$id", Company::class);
     }
 
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
     public function update(Company $company): Company
     {
         return $this->client->putDto("api/teams/$company->id", $company);
@@ -25,6 +38,7 @@ class CompaniesRepository extends Repository
 
     /**
      * @throws GuzzleException
+     * @throws UnknownProperties
      */
     public function representation(): array
     {
